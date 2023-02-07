f7297f90516177ed38289b99775113e89c2d294e
zyrotec <louisno28@gmail.com>
2023-02-07 23:35:03 +0200

    added caption icons added main window title bar with icon, title and caption buttons added layout off main window

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MainWindowModule } from './windows/main-window/main-window.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MainWindowModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/windows/main-window/main-window.component.ts
+++ src/app/windows/main-window/main-window.component.ts
@@ -7,13 +7,16 @@ import { ElectronService } from 'src/app/@core/services/electron.service';
   styleUrls: ['./main-window.component.scss']
 })
 export class MainWindowComponent {
+  public windowTitle: string = 'Zyrotec Electron';
+
+  public isFullscreen!: boolean;
+  public isMenuButton!: boolean;
+
   constructor(private _electronService: ElectronService){}
 
   show(): void {
     this._electronService.ipcRenderer.send('show-message-dialog')
   }
 
-  ngOnInit(): void{
-    this._electronService.ipcRenderer.send('show-message-dialog')
-  }
+  ngOnInit(): void{}
 }
